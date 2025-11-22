9a3d6583835c59c29551939b2b5b4f8c178c6923
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2025-11-22 15:16:04 +0100

    Bean lifecycle hook

--- src/main/java/com/anjolaadeuyi/store_app/OrderService.java
+++ src/main/java/com/anjolaadeuyi/store_app/OrderService.java
@@ -1,5 +1,8 @@
 package com.anjolaadeuyi.store_app;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,23 @@ public class OrderService {
     @Autowired
     public OrderService(@Qualifier("stripe") PaymentService paymentService) {
         this.paymentService = paymentService;
+        System.out.println("OrderService Created");
+    }
+
+    @PostConstruct
+    public void init(){
+        // this is a lifecycle method that is called after the bean is constructed
+        // it is used to initialize the bean
+        // it is called after the bean is constructed and before the bean is used
+        System.out.println("OrderService PostConstruct");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        // this is a lifecycle method that is called before the bean is destroyed
+        // it is used to clean up the bean
+        // it is called before the bean is destroyed and after the bean is used
+        System.out.println("OrderService PreDestroy");
     }
 
     public void placeOrder () {
