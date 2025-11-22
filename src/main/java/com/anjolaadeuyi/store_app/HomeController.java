fb558bfbcf90251e9287bbcc6d53dbec6b440730
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2025-11-22 14:27:52 +0100

    external configurations

--- src/main/java/com/anjolaadeuyi/store_app/HomeController.java
+++ src/main/java/com/anjolaadeuyi/store_app/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @Value("${app.gae-size}")
+    @Value("${spring.application.name}")
     private String appName;
 
     @RequestMapping("/")
