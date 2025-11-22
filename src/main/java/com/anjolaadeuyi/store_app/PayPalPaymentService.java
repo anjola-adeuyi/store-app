7096be3666f028846b005a74c0de006a9073e8f1
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2025-11-22 14:46:21 +0100

    configuring bean using code

--- src/main/java/com/anjolaadeuyi/store_app/PayPalPaymentService.java
+++ src/main/java/com/anjolaadeuyi/store_app/PayPalPaymentService.java
@@ -2,7 +2,7 @@ package com.anjolaadeuyi.store_app;
 
 import org.springframework.stereotype.Service;
 
-@Service("paypal")
+//@Service("paypal")
 public class PayPalPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
