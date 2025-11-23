b373a253a21cde8088284057a72dc75b387b290f
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2025-11-23 06:21:44 +0100

    feat: add profiles and user_tag join tables

---
+++ src/main/resources/db/migration/V4__add_profiles_table.sql
@@ -0,0 +1,9 @@
+-- profiles table
+CREATE TABLE profiles (
+    id BIGINT PRIMARY KEY,
+    bio TEXT,
+    phone_number VARCHAR(15),
+    date_of_birth DATE,
+    loyalty_points INT UNSIGNED DEFAULT 0,
+    FOREIGN KEY (id) REFERENCES users(id)
+);
